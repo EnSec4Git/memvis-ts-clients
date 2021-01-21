8e84e1c212233e17310a75276790e3fef6181927
Yavor <yavor@localhost>
2021-01-21 23:26:42 +0200

    Added a super simple test. In need of Unit tests+mocking and E2E tests in e.g. docker-compose with actual memvis server

---
+++ test/wip.ts
@@ -0,0 +1,22 @@
+import mvc, { MapRow } from '../src/index'
+import net from 'net'
+
+async function main() {
+    let client = new mvc.TCPMVClient("127.0.0.1", 2160, net.Socket)
+    await client._connect()
+    await client.getPtrSize()
+    const mapState = await client.getMaps()
+    console.log(mapState)
+    let nfm: MapRow;
+    for(let mp of mapState.maps) {
+        if(mp.type == 'USED') {
+            nfm = mp
+            break
+        }
+    }
+    const memRow = await client.memr(nfm.start, nfm.start + BigInt(200))
+    console.log(memRow)
+    process.exit(0)
+}
+
+main()
